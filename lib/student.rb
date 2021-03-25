66d8fe4d3fbe346398ed7d6fc11d2a218125429e
Sam D <samidiaz3@gmail.com>
2021-03-25 03:13:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,11 @@
-class Student
+class Student #parent/super
 
   def hello
-    puts "Hey there! I'm so excited to learn stuff."    
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
   end
 
 end
